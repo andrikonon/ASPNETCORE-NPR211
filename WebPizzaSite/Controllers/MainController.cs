b0f21d8af30a5606c084f1a9270921993793edda
andrikonon <andri.konon@gmail.com>
2024-07-14 09:20:43 +0200

    Basic MVC

---
+++ WebPizzaSite/Controllers/MainController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebPizzaSite.Controllers;
+
+public class MainController : Controller
+{
+    public IActionResult Index()
+    {
+        
+        return View();
+    }
+}
